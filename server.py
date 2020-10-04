04f7a7ef42c7ec453fc6e4739190f3fba15f7861
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-10-04 22:02:32 +0530

    feat: remove comments

--- server.py
+++ server.py
@@ -1,111 +1,42 @@
-"""
- ! SERVER File
- ? Bind to socket and listen for client message.
-
- Project by Abhinav Robinson
-"""
-
-
-# Import python socket, random, sympy, hashlib, pickle module
 import socket, random, sympy, hashlib, pickle
-
-# Connect and recieve message
 def recieve():
-
-    # Take prime upper bound
     PRIME_LIMIT = int(input("Enter PRIME the upper bound (1000,1000000000) - (Higher is better): "))
-   
-    # Generate Key Pair
     e,d,n = generate(sympy.randprime(1,PRIME_LIMIT),sympy.randprime(1,PRIME_LIMIT))
-
-    # Show keys
     print(f"Encryption Key: {e} \n Mod Key: {n} \n Secret Key: {d}")
-
-    # Initialize socket stream
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Indicate initialisation
     print("Socket stream initialised as TCP/IP")
-    
-    # Specify port
     port = 6000
-    
-    # Bind to port 
     s.bind((socket.gethostname(), port))
-
-    # Listen for 1 client connection
     s.listen(1)
-
-    # Server init message
     print(f"Server initialised and listening to Port:{port}")
-
-    # Listen for client message
     while True:
- 
-        # Get client socket and address
         clientsocket, address = s.accept()
-
-        # Print Connection details
         print(f"Connection to {address} established!")
 
-        # Send Encryption key (public)
         clientsocket.send(bytes(str(e),"utf-8"))
-
-        # Send Mod Key (public)
         clientsocket.send(bytes(str(n),"utf-8"))
-  
-        # Recieve pickle data stream
         data_string = clientsocket.recv(4096)
-
-        # Recieve Original Hex value (debug for verification)
         hex_encoded = clientsocket.recv(1024)
-
-        # Decode Hex from UTF-8
         public_hex_value = hex_encoded.decode("utf-8")
-
-        # Pickle -> array
         signed_msg = pickle.loads(data_string)
 
-        # Give confirmation
         print(f"Authentication Status: True \n Recieved: {signed_msg}")
-
-        # Decrypt using private key
         hex_value = decrypt(signed_msg, d, n)
-
-        # Show decrypted Hex Value
         print(f"Decrypted Hash Value: {hex_value}")
-
-        # Integrity Check
         if hex_value == public_hex_value: 
-            # Success
             print("Integrity Status : True")
             break
-    #End of while loop
-
-    # Close Connections / Cleanup
     clientsocket.close()
     s.close()
-    
-# EOF
 
 
-# Decrpytes Hex from RSA Scheme
 def decrypt(ctext,d,n):
 
     try:
-        # Decrypt Hex Array from Encrypted Array
         text = [chr(pow(int(char),d,n)) for char in ctext]
-
-        # Returns String
         return "".join(text)
-
-    # Catch Exception
     except TypeError as e: print(e)
 
-# EOF
-
-
-### GENERATE PUBLIC PRIVATE KEY (UNCOMMENTED) ###
 
 def generate(p_num1,p_num2,key_size = 128):
     
@@ -176,10 +107,4 @@ def isPrime(num):
 
     return True
 
-### End Of Key Generation ###
-
-
-# Run function for connection
 recieve()
-
-# End of server.py
